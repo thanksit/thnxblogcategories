f4923f7d87faa1d7c4d4c0510d6032130518c7e3
kamruljpi <kamruljpi@gmail.com>
2018-01-27 18:36:50 +0600

    changes something

--- thnxblogcategories.php
+++ thnxblogcategories.php
@@ -69,9 +69,9 @@ class ThnxBlogCategories extends Module implements WidgetInterface {
         }
         $languages = Language::getLanguages(false);
         foreach ($languages as $lang) { 
-            Configuration::updateValue('thnxbc_title_'.$lang['id_lang'],"Blog Categories");
+            Configuration::updateValue('thnxbc_title_'.$lang['id_lang'], "Blog Categories");
         }
-        Configuration::updateValue('thnxbc_tagcount',4);
+        Configuration::updateValue('thnxbc_tagcount', 4);
         return true;
     }
     // For uninstallation service
@@ -147,9 +147,9 @@ class ThnxBlogCategories extends Module implements WidgetInterface {
         if (Tools::isSubmit('save' . $this->name)) { 
             $languages = Language::getLanguages(false);
             foreach ($languages as $lang) { 
-                Configuration::updateValue('thnxbc_title_'.$lang['id_lang'],Tools::getvalue('thnxbc_title_'.$lang['id_lang']));
+                Configuration::updateValue('thnxbc_title_'.$lang['id_lang'], Tools::getvalue('thnxbc_title_'.$lang['id_lang']));
             }
-            Configuration::updateValue('thnxbc_tagcount',Tools::getvalue('thnxbc_tagcount'));
+            Configuration::updateValue('thnxbc_tagcount', Tools::getvalue('thnxbc_tagcount'));
         }
         $helper = $this->SettingForm();
         $html .= $helper->generateForm($this->fields_form);
@@ -158,7 +158,7 @@ class ThnxBlogCategories extends Module implements WidgetInterface {
     public function renderWidget($hookName = null, array $configuration = array()) 
     { 
         if (Module::isInstalled('thnxblog')  && Module::isEnabled('thnxblog')) { 
-            $this->smarty->assign($this->getWidgetVariables($hookName,$configuration));
+            $this->smarty->assign($this->getWidgetVariables($hookName, $configuration));
             return $this->fetch('module:'.$this->name.'/views/templates/front/ThnxBlogCategories.tpl');
         } else { 
             return false;
@@ -172,7 +172,7 @@ class ThnxBlogCategories extends Module implements WidgetInterface {
             $thnxbc_title = Configuration::get('thnxbc_title_'.$id_lang);
             $thnxbc_tagcount = Configuration::get('thnxbc_tagcount');
             $ThnxBlogCategories = array();
-            $ThnxBlogCategories = thnxpostsclass::GetBlogTags($thnxbc_tagcount,'category');
+            $ThnxBlogCategories = thnxpostsclass::GetBlogTags($thnxbc_tagcount, 'category');
             return array(
                 'thnxbc_title' => $thnxbc_title,
                 'thnxbc_tagcount' => $thnxbc_tagcount,
@@ -202,97 +202,97 @@ class ThnxBlogCategories extends Module implements WidgetInterface {
     public function register_css() 
     { 
         if (isset($this->css_files)  && !empty($this->css_files)) { 
-        $theme_name = $this->context->shop->theme_name;
-        $page_name = $this->context->controller->php_self;
-        $root_path = _PS_ROOT_DIR_.'/';
-        $css_file = array();
-        foreach ($this->css_files as $css_file) :
-            if (isset($css_file['key'])  && !empty($css_file['key'])  && isset($css_file['src'])  && !empty($css_file['src'])) { 
-                $media = (isset($css_file['media'])  && !empty($css_file['media']))  ? $css_file['media'] : 'all';
-                $priority = (isset($css_file['priority'])  && !empty($css_file['priority']))  ? $css_file['priority'] : 50;
-                $page = (isset($css_file['page'])  && !empty($css_file['page']))  ? $css_file['page'] : array('all');
-                if (is_array($page)) { 
-                    $pages = $page;
-                } else { 
-                    $pages = array($page);
-                }
-                if (in_array($page_name, $pages)  || in_array('all', $pages)) { 
-                    if (isset($css_file['load_theme'])  && ($css_file['load_theme'] == true)) { 
-                        $theme_file_src = 'themes/'.$theme_name.'/assets/css/'.$css_file['src'];
-                        if (self::isEmptyFileContet($root_path.$theme_file_src)) { 
-                            $this->context->controller->registerStylesheet(
-                            $css_file['key'],
-                            $theme_file_src,
-                                array(
-                                    'media' => $media,
-                                    'priority' => $priority
-                                ) 
-                            );
-                        }
+            $theme_name = $this->context->shop->theme_name;
+            $page_name = $this->context->controller->php_self;
+            $root_path = _PS_ROOT_DIR_.'/';
+            $css_file = array();
+            foreach ($this->css_files as $css_file) :
+                if (isset($css_file['key'])  && !empty($css_file['key'])  && isset($css_file['src'])  && !empty($css_file['src'])) { 
+                    $media = (isset($css_file['media'])  && !empty($css_file['media']))  ? $css_file['media'] : 'all';
+                    $priority = (isset($css_file['priority'])  && !empty($css_file['priority']))  ? $css_file['priority'] : 50;
+                    $page = (isset($css_file['page'])  && !empty($css_file['page']))  ? $css_file['page'] : array('all');
+                    if (is_array($page)) { 
+                        $pages = $page;
                     } else { 
-                        $module_file_src = 'modules/'.$this->name.'/views/css/'.$css_file['src'];
-                        if (self::isEmptyFileContet($root_path.$module_file_src)) { 
-                            $this->context->controller->registerStylesheet(
-                            $css_file['key'],
-                            $module_file_src,
-                                array(
-                                    'media' => $media,
-                                    'priority' => $priority
-                                ) 
-                            );
+                        $pages = array($page);
+                    }
+                    if (in_array($page_name, $pages)  || in_array('all', $pages)) { 
+                        if (isset($css_file['load_theme'])  && ($css_file['load_theme'] == true)) { 
+                            $theme_file_src = 'themes/'.$theme_name.'/assets/css/'.$css_file['src'];
+                            if (self::isEmptyFileContet($root_path.$theme_file_src)) { 
+                                $this->context->controller->registerStylesheet(
+                                $css_file['key'],
+                                $theme_file_src,
+                                    array(
+                                        'media' => $media,
+                                        'priority' => $priority
+                                    ) 
+                                );
+                            }
+                        } else { 
+                            $module_file_src = 'modules/'.$this->name.'/views/css/'.$css_file['src'];
+                            if (self::isEmptyFileContet($root_path.$module_file_src)) { 
+                                $this->context->controller->registerStylesheet(
+                                $css_file['key'],
+                                $module_file_src,
+                                    array(
+                                        'media' => $media,
+                                        'priority' => $priority
+                                    ) 
+                                );
+                            }
                         }
                     }
                 }
-            }
-        endforeach;
+            endforeach;
         }
         return true;
     }
     public function register_js() 
     { 
         if (isset($this->js_files)  && !empty($this->js_files)) { 
-        $theme_name = $this->context->shop->theme_name;
-        $page_name = $this->context->controller->php_self;
-        $root_path = _PS_ROOT_DIR_.'/';
-        foreach ($this->js_files as $js_file) :
-            if (isset($js_file['key'])  && !empty($js_file['key'])  && isset($js_file['src'])  && !empty($js_file['src'])) { 
-                $position = (isset($js_file['position'])  && !empty($js_file['position']))  ? $js_file['position'] : 'bottom';
-                $priority = (isset($js_file['priority'])  && !empty($js_file['priority']))  ? $js_file['priority'] : 50;
-                $page = (isset($css_file['page'])  && !empty($css_file['page']))  ? $css_file['page'] : array('all');
-                if (is_array($page)) { 
-                    $pages = $page;
-                } else { 
-                    $pages = array($page);
-                }
-                if (in_array($page_name, $pages)  || in_array('all', $pages)) { 
-                    if (isset($js_file['load_theme'])  && ($js_file['load_theme'] == true)) { 
-                        $theme_file_src = 'themes/'.$theme_name.'/assets/js/'.$js_file['src'];
-                        if (self::isEmptyFileContet($root_path.$theme_file_src)) { 
-                            $this->context->controller->registerJavascript(
-                            $js_file['key'],
-                            $theme_file_src,
-                                array(
-                                    'position' => $position,
-                                    'priority' => $priority
-                                ) 
-                            );
-                        }
+            $theme_name = $this->context->shop->theme_name;
+            $page_name = $this->context->controller->php_self;
+            $root_path = _PS_ROOT_DIR_.'/';
+            foreach ($this->js_files as $js_file) :
+                if (isset($js_file['key'])  && !empty($js_file['key'])  && isset($js_file['src'])  && !empty($js_file['src'])) { 
+                    $position = (isset($js_file['position'])  && !empty($js_file['position']))  ? $js_file['position'] : 'bottom';
+                    $priority = (isset($js_file['priority'])  && !empty($js_file['priority']))  ? $js_file['priority'] : 50;
+                    $page = (isset($css_file['page'])  && !empty($css_file['page']))  ? $css_file['page'] : array('all');
+                    if (is_array($page)) { 
+                        $pages = $page;
                     } else { 
-                        $module_file_src = 'modules/'.$this->name.'/views/js/'.$js_file['src'];
-                        if (self::isEmptyFileContet($root_path.$module_file_src)) { 
-                            $this->context->controller->registerJavascript(
-                            $js_file['key'],
-                            $module_file_src,
-                                array(
-                                    'position' => $position,
-                                    'priority' => $priority
-                                ) 
-                            );
+                        $pages = array($page);
+                    }
+                    if (in_array($page_name, $pages)  || in_array('all', $pages)) { 
+                        if (isset($js_file['load_theme'])  && ($js_file['load_theme'] == true)) { 
+                            $theme_file_src = 'themes/'.$theme_name.'/assets/js/'.$js_file['src'];
+                            if (self::isEmptyFileContet($root_path.$theme_file_src)) { 
+                                $this->context->controller->registerJavascript(
+                                $js_file['key'],
+                                $theme_file_src,
+                                    array(
+                                        'position' => $position,
+                                        'priority' => $priority
+                                    ) 
+                                );
+                            }
+                        } else { 
+                            $module_file_src = 'modules/'.$this->name.'/views/js/'.$js_file['src'];
+                            if (self::isEmptyFileContet($root_path.$module_file_src)) { 
+                                $this->context->controller->registerJavascript(
+                                $js_file['key'],
+                                $module_file_src,
+                                    array(
+                                        'position' => $position,
+                                        'priority' => $priority
+                                    ) 
+                                );
+                            }
                         }
                     }
                 }
-            }
-        endforeach;
+            endforeach;
         }
         return true;
     }
